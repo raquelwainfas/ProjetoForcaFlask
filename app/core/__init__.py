384a49d6b22f5da66f0a789d05edc8ce841fd106
Dev3rt0n <everton.osle@gmail.com>
2020-08-03 00:17:47 -0300

    Atualizacao da interface Forca

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,42 +1,16 @@
 # -*- coding: utf-8 -*-
 from .main import categorias
+from flask import session
 
 class Forca:
     def __init__(self, categoria):
         self.opcao = categorias[categoria]()
+        session['palavra'] = self.opcao.palavra_sorteada
 
-    def palavra(self):
-        return self.opcao.palavra_sorteada
+    @staticmethod
+    def verificar(letra):
+        pos = [i+1 for i, l in enumerate(session['palavra'].lower()) if l == letra.lower()]
 
-
-if __name__ == '__main__':
-
-    with open("Animais.txt", "r", encoding="utf8") as animais:
-        sorteio = animais.read().split('\n')
-        tam = len(sorteio)
-        palavra_sorteada = (sorteio[random.randrange(0,tam)]) #noqa
-    # print(palavra_sorteada.upper()+'\n')
-
-    qtd_letras = (len(palavra_sorteada))
-    #print(qtd_letras)
-    escondida = qtd_letras * '_'
-    print(qtd_letras * '_ ')
-
-    lista_palavra = list(palavra_sorteada.upper())
-    escondida = list(escondida)
-
-    # print(lista_palavra)
-    # print(escondida)
-
-    while "_" in escondida:
-
-        letra_ipt = input("Digite uma letra: ")
-
-        for index in range(len(lista_palavra)):
-            if letra_ipt.upper() in lista_palavra[index]:
-                #print(index)
-                escondida[index] = letra_ipt.upper()
-                #print(escondida)
-                final = " ".join(escondida)
-        os.system('cls') #noqa
-        print(final)
+        if pos:
+            return {'status': True, 'pos':pos}
+        return {'status': False}
